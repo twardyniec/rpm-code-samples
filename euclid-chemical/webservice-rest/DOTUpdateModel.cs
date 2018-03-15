871cf75fd5f0264809bd5625484ae08e5ea825b3
twardyniec <37072208+twardyniec@users.noreply.github.com>
2018-03-15 13:25:53 -0400

    Add files via upload

---
+++ euclid-chemical/webservice-rest/DOTUpdateModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Euclid7.Models
+{
+    public class DOTUpdateModel
+    {
+        public string updateType { get; set; }
+        public List<StateData> data { get; set; }
+    }
+
+    public class StateData
+    {
+        public string state { get; set; }
+        public string product { get; set; }
+        public int id { get; set; }
+    }
+}
